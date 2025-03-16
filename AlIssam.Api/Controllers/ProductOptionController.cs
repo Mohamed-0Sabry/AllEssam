941952df8871c9549407ff995c64b8972126b196
mustafa <mustafaahmedelshaer@outlook.com>
2025-03-16 21:09:51 +0300

    add productOptionUpdateEndPoint

---
+++ AlIssam.Api/Controllers/ProductOptionController.cs
@@ -0,0 +1,30 @@
+﻿using AlIssam.Api.Services.interFaces;
+using AlIssam.API.Dtos.Response;
+using AlIssam.DataAccessLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlIssam.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductOptionController(IproductOptionService service) : ControllerBase
+    {
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Update(int Id,ProductOptions request)
+        {
+            var result = await service.UpdateProductOptionAsync(Id, request);
+            return HandleServiceResult(result);
+
+        }
+
+        private IActionResult HandleServiceResult<T>(
+         (bool isSuccess, ErrorResponse? errorResponse, T? response) result)
+        {
+            if (!result.isSuccess || result.errorResponse != null)
+                return BadRequest(result.errorResponse);
+
+            return Ok(result.response);
+        }
+    }
+}
---
+++ AlIssam.Api/Services/ProductOptionService.cs
@@ -0,0 +1,36 @@
+﻿using AlIssam.Api.Services.interFaces;
+using AlIssam.API.Dtos.Response;
+using AlIssam.API.Extension;
+using AlIssam.DataAccessLayer;
+using AlIssam.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlIssam.Api.Services
+{
+    public class ProductOptionService(AlIssamDbContext dbContext) : IproductOptionService
+    {
+        public async Task<(bool isSuccess, ErrorResponse? errorMessage, int? productOptionId)> UpdateProductOptionAsync(int id, ProductOptions request)
+        {
+            var option = await dbContext.ProductsOptions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (option is null)
+                return (false, new ErrorResponse().CreateErrorResponse("لا يوجد خيارات للمنتج بهذا المعرف", "not found"), null);
+
+
+            option.Offer = request.Offer;
+            option.Price = request.Price;
+            option.OrderDetailsList = request.OrderDetailsList;
+            option.Default = request.Default;
+
+            dbContext.ProductsOptions.Update(option);
+
+            if (await dbContext.SaveChangesAsync() > 0) 
+            {
+                return (true, null, id);
+            }
+
+            return(false, new ErrorResponse().CreateErrorResponse("حدث خطأ", "An error occurred ."), null);
+
+        }
+    }
+}
